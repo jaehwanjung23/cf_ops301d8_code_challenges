e20b51034c6716cf1ae7af96c1e67c34de4edaef
jaehwanjung23 <58351858+jaehwanjung23@users.noreply.github.com>
2023-06-07 11:48:34 -0400

    Create bashinpython.py

---
+++ bashinpython.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+# Script Name:               Bash in Python
+# Author:                    Jason J
+# Date of latest revision:   6/6/2023
+# Purpose:                   Python script that executes a few bash commands successfully. Indicate in comments how you achieved this.
+#                            Requirements:
+#                            The Python module “os” must be utilized
+#                            At least three variables must be declared and referenced in Python
+#The Python function print() must be used at least three times
+# Instructions:
+#The Python module “os” must be utilized
+#At least three variables must be declared and referenced in Python
+#The Python function print() must be used at least three times
+#Include execution of the following bash commands inside your Python script:
+#use these commands
+#whoami
+#ip a
+#lshw -short
+
+#Declartion of varibles:
+import os
+# to call this variable do put the defined variable word in () like this (variable)
+user = "whoami"
+ip = "ip -a"
+shortLshw = "lshw -short"
+
+#Declaration of functions:
+
+
+#Main
+
+print(os.system(IP))
+print(os.system(user))
+print(os.system(show))
